5a49ca687401c2b18f87db73cdf2bb6b3056e2eb
Amoong <moo1323@naver.com>
2024-04-04 16:56:27 +0900

    Add crash and finish particle

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = .5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
