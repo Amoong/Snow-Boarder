ac9415626c3e2811c35d42a2bd31f66df1909f39
Amoong <moo1323@naver.com>
2024-04-05 20:02:50 +0900

    Add crash and finish SFX

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,12 +7,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = .5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", reloadDelay);
         }
     }
